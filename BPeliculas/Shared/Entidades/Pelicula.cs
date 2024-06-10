cc4788d0cfeedeb566f3561b0fc15af4c5826bf6
Fernando Nicolás <jnicolas@previvale.mx>
2024-06-10 11:36:00 -0600

    Introducción a Blazor.

---
+++ BPeliculas/Shared/Entidades/Pelicula.cs
@@ -0,0 +1,7 @@
+﻿
+namespace BPeliculas.Shared.Entidades {
+    public class Pelicula {
+        public string Titulo { get; set; } = null!;
+        public DateTime FechaLanzamiento { get; set; }
+    }
+}
